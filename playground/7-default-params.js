5b66bdc8b179f59cfa64dfe777963c392407fb36
Azat Seyrek <azatseyrek@hotmail.com>
2022-01-04 01:23:27 +0300

    server side connection

---
+++ playground/7-default-params.js
@@ -0,0 +1,5 @@
+const greeter = (name="miran") => {
+    console.log("Hello " + name);
+}
+
+greeter()
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,9 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
 const app = express();
 // console.log(path.join(__dirname, "../public"));
 
@@ -13,18 +16,15 @@ app.set("view engine", "hbs");
 // static olarak html sayfalarimizi cagirdigimizda artik app.get methodu ile(routerlari) cagirmamiza gerek kalmiyor.
 app.use(express.static(publicDirectoryPath));
 
-
-
 //So there's a views configuration for express. The default value for that is ./views (docs). So if you run the node app from inside the src/ folder, it's looking for src/views/ folder. If you run the Node app from the root of the project, it's looking for views/ which does exist.
 // app.set("views", path.join(__dirname, "../views"));
 //hbs default olarak views kullaniyor. bunu degistirmek icin ornegin biz views  degilde template yapmak istiyoruz dosyomizin ismini. O zman soyle bir yol izliyoruz
-const viewPath = path.join(__dirname, '../templates/views')
-app.set('views', viewPath)
+const viewPath = path.join(__dirname, "../templates/views");
+app.set("views", viewPath);
 
 //partials path olusturalim
-const partialsPath = path.join(__dirname, "../templates/partials")
-hbs.registerPartials(partialsPath)
-
+const partialsPath = path.join(__dirname, "../templates/partials");
+hbs.registerPartials(partialsPath);
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -35,24 +35,71 @@ app.get("/", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    title: "This is Help Page",
-    name: "Miran Ali Seyrek"
+    title: "Help Page",
+    name: "Miran Ali Seyrek",
   });
 });
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "This is About Page",
-    name: "Burcu Seyrek"
+    title: "About Me",
+    name: "Burcu Seyrek",
   });
 });
 app.get("/weather", (req, res) => {
-  res.send("Weather Page!");
+  if (!req.query.address) {
+    return res.send({
+      error: "There is no address information",
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+
+  // res.send({
+  //   forecast: "It is snowing",
+  //   location: "philadelphia",
+  //   address: req.query.address,
+  // });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term!",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+//herhangi bir route yoksa kendi mesajimizi gosterebiliriz * ile
+app.get("*", (req, res) => {
+  res.send("My 404 Page");
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404");
 });
 
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-
 //onemli not
 //nodemon hbs degistiginde tekrar calismiyor. Bunu ayarlamak icin terminale extension yukleyebiliriz.
-// nodemon app.js -e js,hbs  
+// nodemon app.js -e js,hbs
