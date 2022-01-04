6db815d974e1297bab7e7f2e0f9618b3b6685bbd
Azat Seyrek <azatseyrek@hotmail.com>
2022-01-05 00:39:21 +0300

    rendering data to hbs

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,31 @@
 console.log("client side js file loaded");
 
-fetch("http://localhost:3000/weather?address=mersin").then((response) => {
-  response.json().then((data) => {
-    if(data.error) {
-        console.log(data.error);
-    }else {
-        console.log(data.location);
-        console.log(data.forecast);
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById("message-1");
+const messageTwo = document.getElementById("message-2");
+
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  // console.log(location)
+  
+  messageOne.innerHTML 
+  messageTwo.innerHTML
+
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          // console.log(data.location);
+          // console.log(data.forecast);
+          messageOne.innerHTML = data.location
+          messageTwo.innerHTML = data.forecast
+        }
+      });
     }
-  });
+  );
 });
